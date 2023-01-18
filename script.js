c695b74ff5875b353fe176685047efd636f565c6
Connor Vosberg <connorjv@gmail.com>
2023-01-18 14:16:57 -0800

    'submit' event handler is working.  Fetch request from the Bored API is currently not working.

---
+++ script.js
@@ -0,0 +1,40 @@
+// document.addEventListener("DOMContentLoaded", initialize)
+
+// function initialize(e) {
+//     e.preventDefault()
+// }
+
+// let activityType 
+
+function getActivity(type) {
+    fetch(`https://www.boredapi.com//api/activity?type=${type}`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify()
+    })
+    .then(res => res.json())
+    .then(data => {
+        // activityType = data
+        console.log(data)
+    })
+}
+
+
+let search = document.querySelector('#activity-search');
+search.addEventListener('submit', handleSearch)
+
+
+function handleSearch(e) {
+    e.preventDefault()
+    const input = document.querySelector('#search')
+    let type = input.value
+    console.log(type)
+    getActivity(type)
+}
+
+
+// function renderActivity(activity) {
+
+// }
